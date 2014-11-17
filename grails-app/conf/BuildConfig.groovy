78f0724601e45b0da6f7de89f99b51d8d0a57b88
Cristan Meijer <cristan.meijer@gmail.com>
2014-11-17 01:49:52 +0100

    Copied some values from a new project (which might have helped in prevented a NullPointerException)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -69,12 +69,11 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.5.5" // or ":hibernate4:4.1.11.6"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
-        //runtime ":resources:1.2.13"
-		
-		
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
+        
+		// Uncomment these to enable additional asset-pipeline capabilities
+		//compile ":sass-asset-pipeline:1.9.0"
+		//compile ":less-asset-pipeline:1.10.0"
+		//compile ":coffee-asset-pipeline:1.8.0"
+		//compile ":handlebars-asset-pipeline:1.3.0.3"
     }
 }
