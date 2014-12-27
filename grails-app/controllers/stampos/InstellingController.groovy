1977e6608ff925422e33dbb96b0bd4f32e845b08
Cristan Meijer <cristan.meijer@gmail.com>
2014-12-27 18:49:02 +0100

    Added a setting to allow requests or not and added a nice settings screen to change this setting.

--- grails-app/controllers/stampos/InstellingController.groovy
+++ grails-app/controllers/stampos/InstellingController.groovy
@@ -3,4 +3,17 @@ package stampos
 class InstellingController {
 
     static scaffold = true
+	def settingsService
+	
+	def overzicht()
+	{
+		return ["allowRequests": settingsService.getAllowRequests(), "allowRequestKeys": [SettingsService.LOCALHOST, SettingsService.LOCAL_NETWORK, SettingsService.EVERYWHERE] ]
+	}
+	
+	def submit()
+	{
+		settingsService.setAllowRequests(params.allowRequestsFrom)
+		flash.message = g.message(code: "settings.saved")
+		redirect (action:"overzicht")
+	}
 }
