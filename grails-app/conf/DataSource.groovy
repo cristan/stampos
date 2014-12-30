0eba654a2427f1cc47258d49a63ca4d7a443be03
Cristan Meijer <cristan.meijer@gmail.com>
2014-12-30 22:07:42 +0100

    Made the test DB the default one again.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,34 +17,34 @@ hibernate {
 // environment specific settings
 environments {
     development {
-//        dataSource {
-//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//        }
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-			properties {
-			   // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-			}
-		}
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+        }
+//		dataSource {
+//			dbCreate = "update"
+//			url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//			properties {
+//			   // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+//               jmxEnabled = true
+//               initialSize = 5
+//               maxActive = 50
+//               minIdle = 5
+//               maxIdle = 25
+//               maxWait = 10000
+//               maxAge = 10 * 60000
+//               timeBetweenEvictionRunsMillis = 5000
+//               minEvictableIdleTimeMillis = 60000
+//               validationQuery = "SELECT 1"
+//               validationQueryTimeout = 3
+//               validationInterval = 15000
+//               testOnBorrow = true
+//               testWhileIdle = true
+//               testOnReturn = false
+//               jdbcInterceptors = "ConnectionState"
+//               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+//			}
+//		}
     }
     test {
         dataSource {
