c19a5df8594d3d5a19da6a7d7cf06b03a3cc540e
Cristan Meijer <cristan.meijer@gmail.com>
2015-10-03 15:59:49 +0200

    Updated all plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,8 +67,8 @@ grails.project.dependency.resolution = {
 		compile ":spring-websocket:1.3.0"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.6.1"
-        runtime ":database-migration:1.4.0"
+        runtime ":hibernate4:4.3.8.1"
+        runtime ":database-migration:1.4.1"
         runtime ":jquery:1.11.1"
         
 		// Uncomment these to enable additional asset-pipeline capabilities
