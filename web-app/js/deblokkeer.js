aa72d90ea547bd72fff14ff9a62978f291a45716
Cristan Meijer <cristan.meijer@gmail.com>
2015-10-27 22:01:46 +0100

    This should fix deblokkeer

--- web-app/js/deblokkeer.js
+++ web-app/js/deblokkeer.js
@@ -7,7 +7,7 @@ function logIn(password)
 	$("#passwordField").css("border", "2px solid white");
 
 	
-	$.getJSON('deblokkeer/logIn?password='+ password, function(data) {
+	$.getJSON('logIn?password='+ password, function(data) {
 		if(!data.loggedIn)
 		{
 			$("#passwordField").addClass("incorrectAnimation");
@@ -23,7 +23,7 @@ function logIn(password)
 		}
 	});
 
-	$.getJSON('deblokkeer/blockedUsers', function(data) {
+	$.getJSON('blockedUsers', function(data) {
 		klanten = data;
 		tekenKlanten();
 		dataLoaded = true;
