fb6122cfa274c79f3c19e78a9624adfb2fb15498
Cristan Meijer <c.meijer@itude.com>
2016-10-27 11:53:57 +0200

    Started working on the refactoring.

--- grails-app/controllers/stampos/KlantinfoController.groovy
+++ grails-app/controllers/stampos/KlantinfoController.groovy
@@ -33,32 +33,37 @@ class KlantinfoController {
 		}
 		Date beginDatum = params.beginDatum ? new Date(params.beginDatum as long) : null
 		
-		String query = "from Bestelling b"
-		def namedParams = [:]
-		if(klant)
+		List<Bestelling> besteld;
+		def queryParams = [max: maxItems + 1, sort:"datum"]
+		if(klant) 
 		{
-			query += " where b.klant = :klant and"
-			namedParams = [klant:klant] 
+			if(beginDatum) 
+			{
+				besteld = Bestelling.findAllByKlantAndDatumLessThan(klant, beginDatum, queryParams)
+			}
+			else 
+			{
+				besteld = Bestelling.findAllByKlant(klant, queryParams)
+			}
+		} 
+		else 
+		{
+			if(beginDatum) 
+			{
+				besteld = Bestelling.findAllByDatumLessThan(beginDatum, queryParams)
+			}
+			else 
+			{
+				besteld = Bestelling.findAll(queryParams)
+			}
 		}
 		 
-		if(beginDatum)
-		{
-			if(!klant)
-			{
-				query += " where"
-			}
-			query += " b.datum < :eersteBestellingDatum"
-			namedParams.put("eersteBestellingDatum", beginDatum)
-		}
-		query += " order by b.datum desc"
-		List<Bestelling> besteld = Bestelling.findAll (query, namedParams, [max: maxItems + 1])
 		boolean nogOverigeBestellingen = besteld.size() > maxItems
 		if(nogOverigeBestellingen)
 		{
 			// Deze (en daarop volgenden) worden de volgende keer geladen
 			besteld.remove(besteld.size() - 1)
 		}
-		
 		
 		def items = [];
 		for(Bestelling bestelling: besteld)
@@ -72,43 +77,36 @@ class KlantinfoController {
 		{
 			testDataService.getTestBetalingen()
 		}
-		query = "from Betaling b where"
-		namedParams = [:]
-		if(klant)
-		{
-			query += " b.klant = :klant and"
-			namedParams.put("klant", klant)
-		}
-		def queryEnd = " order by b.datum desc"
-		List<Betaling> betaald
-		
+
+		List<Betaling> betaald		
 		boolean nogOverigeBetalingen = false;
+		queryParams = [max: maxItems + 1, sort:"datum", order: "desc"]
 		if(nogOverigeBestellingen)
 		{
 			// De bestellingen zijn genoeg om maxItems te halen. Haal alle bestellingen die tussen de opgehaalde bestellingen vallen
-			query += " b.datum > :laatsteBestellingDatum" 
-			namedParams.put("laatsteBestellingDatum", besteld.get(maxItems -1).datum)
-			if(beginDatum)
-			{
-				query += " and b.datum < :eersteBestellingDatum"
-				namedParams.put("eersteBestellingDatum", beginDatum)
+			
+			def laatsteBestellingDatum = besteld.get(maxItems -1).datum
+			if(beginDatum) {
+				betaald = Betaling.findAllByDatumBetween(beginDatum, laatsteBestellingDatum, queryParams)
+			} else {
+				betaald = Betaling.findAllByDatumLessThan(laatsteBestellingDatum, queryParams)
 			}
-			query += queryEnd
-			betaald = Betaling.findAll (query, namedParams)
 		}
 		else
 		{
 			// Er zijn niet genoeg bestellingen om maxItems te halen. Daarom geen max aan het einde van de datum
-			namedParams = [:]
-			if(beginDatum)
-			{
-				query += " b.datum < :eersteBestellingDatum"
-				namedParams.put("eersteBestellingDatum", beginDatum)
-			}
-			query += queryEnd
 			def maxBestellingen = maxItems - besteld.size() + 1
-			def queryParams = [max: maxBestellingen]
-			betaald = Betaling.findAll (query, namedParams, queryParams)
+			queryParams.max = maxBestellingen
+			if(beginDatum) {
+				betaald = Betaling.findAllByDatumLessThan(beginDatum, queryParams)
+			} else {
+				println "all:"
+				println Betaling.findAll()
+				println "some:"
+				betaald = Betaling.findAll(max: maxBestellingen, sort:"datum", order: "desc"){}//
+				println betaald
+			}
+
 			nogOverigeBetalingen = betaald.size() == maxBestellingen
 			if(nogOverigeBetalingen)
 			{
@@ -116,6 +114,7 @@ class KlantinfoController {
 				betaald.remove(betaald.size() - 1)
 			}
 		}
+		
 		for(Betaling betaling : betaald)
 		{
 			items.add(klantInfoService.getJsonPayment(betaling, klant == null))
