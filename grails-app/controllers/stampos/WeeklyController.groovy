bf87fcbd18da6415bea913ba3bc3f67a25c9b1c2
Cristan Meijer <cristan.meijer@gmail.com>
2018-01-03 00:10:41 +0100

    Make weeks in the weekly overview go from monday to sunday

--- grails-app/controllers/stampos/WeeklyController.groovy
+++ grails-app/controllers/stampos/WeeklyController.groovy
@@ -8,10 +8,10 @@ class WeeklyController {
 	static DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy")
 
 	def index() {
-		Date today = new Date().clearTime()
-		Date endDate = params.endDate ? new Date(params.endDate as long) : today
-		Date beginDate = params.beginDate ? new Date(params.beginDate as long) : endDate - 7
-
+		Date now = new Date()
+		Date beginDate = params.beginDate ? new Date(params.beginDate as long) : getStartOfWeek(now)
+		Date endDate = params.endDate ? new Date(params.endDate as long) : getEndOfWeek(now)
+		
 		def ordered = BestelRegel.executeQuery("select br.productPrijs.product.naam, sum(br.aantal), sum(br.aantal * br.productPrijs.prijs) from BestelRegel br "+
 			"where br.bestelling.datum > :dateFrom and br.bestelling.datum < :dateTo "+
 			"group by br.productPrijs.product.naam "+
@@ -47,6 +47,49 @@ class WeeklyController {
 			previousEndDate: (endDate - 7).getTime(),
 			nextBeginDate: (beginDate + 7).getTime(), 
 			nextEndDate: (endDate + 7).getTime(),
-			shouldShowLinkToNextWeek: endDate < today]
+			shouldShowLinkToNextWeek: endDate < getEndOfWeek(now)]
 	}
+	
+	private Date getStartOfWeek(Date date) {
+		def cal = Calendar.instance
+		cal.setTime(date);
+		
+		while (cal.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+			cal.add(Calendar.DAY_OF_WEEK, -1)
+		}
+		
+		return getStartOfDay(cal.time)
+	}
+	
+	private Date getEndOfWeek(Date date) {
+		def cal = Calendar.instance
+		cal.setTime(date);
+		
+		while (cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+			cal.add(Calendar.DAY_OF_WEEK, +1)
+		}
+		
+		return getEndOfDay(cal.time)
+	}
+	
+	public Date getEndOfDay(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR_OF_DAY, 23);
+		calendar.set(Calendar.MINUTE, 59);
+		calendar.set(Calendar.SECOND, 59);
+		calendar.set(Calendar.MILLISECOND, 999);
+		return calendar.getTime();
+	}
+	
+	public Date getStartOfDay(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar.getTime();
+	}
+
 }
